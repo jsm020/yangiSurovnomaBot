dc157f2fbad7b9b8e9f4b5117530b50197ad6161
jsm020 <javohirxikmatillaev@gmail.com>
2025-05-14 09:20:13 +0300

    Configure static and media file handling: update settings, add static volume in Docker, and modify URLs for serving media files

--- config/urls.py
+++ config/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("api/students/", include("students.urls")),
 ]
 if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
